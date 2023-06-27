413ae9117710f268538dcd591f5c15e17207b1ec
Artyom Guryanov <bishop81285@gmail.com>
2023-06-27 22:24:01 +0600

    added show_last_executed_trans func

--- develop/utils.py
+++ develop/utils.py
@@ -1,6 +1,7 @@
 import json
 
 from cfg.settings import DATA_JSON
+from develop.trans_class import Transactions
 
 
 def get_data() -> list[dict]:
@@ -12,3 +13,17 @@ def get_data() -> list[dict]:
 
     with open(_path) as file:
         return json.load(file)
+
+
+def show_last_executed_trans(data_trans: list[dict], amount: int = 5) -> None:
+    executed_trans = [el for el in data_trans if 'state' in el and el['state'] == 'EXECUTED']
+    executed_trans_date_sorted = sorted(executed_trans, key=lambda el: el['date'], reverse=True)
+
+    executed_trans_view = []
+
+    for i in range(amount):
+        executed_trans_view.append(Transactions(**executed_trans_date_sorted[i]))
+
+    for trans in executed_trans_view:
+        pass
+
