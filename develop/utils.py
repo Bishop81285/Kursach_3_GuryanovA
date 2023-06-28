ec86ddd3fbd755e497b8b0751ed2189e3f99bee0
Artyom Guryanov <bishop81285@gmail.com>
2023-06-28 13:51:05 +0600

    utils.py module completed

--- develop/utils.py
+++ develop/utils.py
@@ -16,7 +16,18 @@ def get_data() -> list[dict]:
 
 
 def show_last_executed_trans(data_trans: list[dict], amount: int = 5) -> None:
+    """
+    Show last 'amount' transactions that were executed
+    :param data_trans: python-json format data
+    :param amount: number of transactions
+    :return: nothing, just printing
+    """
     executed_trans = [el for el in data_trans if 'state' in el and el['state'] == 'EXECUTED']
+
+    if amount > len(executed_trans):
+        print(f'AmountError: set correct amount of transactions (< {len(executed_trans) + 1})')
+        return
+
     executed_trans_date_sorted = sorted(executed_trans, key=lambda el: el['date'], reverse=True)
 
     executed_trans_view = []
@@ -24,6 +35,35 @@ def show_last_executed_trans(data_trans: list[dict], amount: int = 5) -> None:
     for i in range(amount):
         executed_trans_view.append(Transactions(**executed_trans_date_sorted[i]))
 
-    for trans in executed_trans_view:
-        pass
+    print('EXECUTED:')
 
+    for trans in executed_trans_view:
+        print(trans.view_trans())
+        print('')
+
+
+def show_last_canceled_trans(data_trans: list[dict], amount: int = 5) -> None:
+    """
+    Show last 'amount' transactions that were canceled
+    :param data_trans: python-json format data
+    :param amount: number of transactions
+    :return: nothing, just printing
+    """
+    canceled_trans = [el for el in data_trans if 'state' in el and el['state'] == 'CANCELED']
+
+    if amount > len(canceled_trans):
+        print(f'AmountError: set correct amount of transactions (< {len(canceled_trans) + 1})')
+        return
+
+    canceled_trans_date_sorted = sorted(canceled_trans, key=lambda el: el['date'], reverse=True)
+
+    canceled_trans_view = []
+
+    for i in range(amount):
+        canceled_trans_view.append(Transactions(**canceled_trans_date_sorted[i]))
+
+    print('CANCELED:')
+
+    for trans in canceled_trans_view:
+        print(trans.view_trans())
+        print('')
